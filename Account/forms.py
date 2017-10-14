64c89319c2f55e132c3a65c868c107486d233000
mateuszmarzec <mateuszmikolajmarzec@gmail.com>
2017-10-14 02:36:40 +0200

    Password_reset_betaV2

--- Account/forms.py
+++ Account/forms.py
@@ -1,14 +1,15 @@
-from django.contrib.auth.forms import PasswordResetForm, UserModel
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.exceptions import ValidationError
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-
 from RateApp.models import UserData
+from RateApp import encoding_functions
 
-
+## PasswordResetForm subclass to enable reseting password
 class EmailValidationOnForgotPassword(PasswordResetForm):
     def get_users(self, email):
         """Given an email, return matching user(s) who should receive a reset.
@@ -23,36 +24,20 @@ class EmailValidationOnForgotPassword(PasswordResetForm):
         })
         return (u for u in active_users)
 
-    def save(self, domain_override=None,
-             subject_template_name='registration/password_reset_subject.txt',
-             email_template_name='registration/password_reset_email.html',
-             use_https=False, token_generator=default_token_generator,
-             from_email=None, request=None, html_email_template_name=None,
-             extra_email_context=None):
-        """
-        Generates a one-use only link for resetting password and sends to the
-        user.
-        """
-        email = self.cleaned_data["email"]
-        for user in self.get_users(email):
-            if not domain_override:
-                current_site = get_current_site(request)
-                site_name = current_site.name
-                domain = current_site.domain
-            else:
-                site_name = domain = domain_override
-            context = {
-                'email': email,
-                'domain': domain,
-                'site_name': site_name,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'user': user,
-                'token': token_generator.make_token(user),
-                'protocol': 'https' if use_https else 'http',
-            }
-            if extra_email_context is not None:
-                context.update(extra_email_context)
-            self.send_mail(
-                subject_template_name, email_template_name, context, from_email,
-                email, html_email_template_name=html_email_template_name,
-            )
+## SetPasswordForm subclass to set password in database
+class NewPasswordSave(SetPasswordForm):
+
+    def save(self, commit=True):
+        password = self.cleaned_data["new_password1"]
+        self.user.set_password(password)
+
+        # set password in my database
+        tuple = encoding_functions.create_password_hash(password)
+        db_user = UserData.objects.get(login=self.user)
+        db_user.password_hash = tuple[1]
+        db_user.password_salt = tuple[0]
+
+        if commit:
+            self.user.save()
+            db_user.save()
+        return self.user
--- RateYourSubjects/urls.py
+++ RateYourSubjects/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
         {'template_name': 'registration/password_reset_form.html', 'password_reset_form': forms.EmailValidationOnForgotPassword},
         name="password_reset"),
     url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, {'set_password_form': forms.NewPasswordSave}, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 ]
